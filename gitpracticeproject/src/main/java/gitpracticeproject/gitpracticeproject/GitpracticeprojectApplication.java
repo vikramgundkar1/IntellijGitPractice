32d33173120ec6acfbce5589dd36d5219d29bd3a
vikramgundkar1 <110653893+vikramgundkar1@users.noreply.github.com>
2024-06-13 12:19:47 +0530

    first commit

---
+++ gitpracticeproject/src/main/java/gitpracticeproject/gitpracticeproject/GitpracticeprojectApplication.java
@@ -0,0 +1,13 @@
+package gitpracticeproject.gitpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpracticeprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitpracticeprojectApplication.class, args);
+	}
+
+}
---
+++ gitpracticeproject/src/test/java/gitpracticeproject/gitpracticeproject/GitpracticeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package gitpracticeproject.gitpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpracticeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
